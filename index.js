e872eaf86a574f6f6bb3c64ac3b795734e72b883
mprinceps <64213121+bcp2415@users.noreply.github.com>
2021-10-07 16:57:37 +0000

    adds array to hold animal objects, code to assign astronautID uniquely (needs debugging -- still producing some duplicate IDs).

--- index.js
+++ index.js
@@ -12,8 +12,43 @@ let salamander = {
     age: 5
 };
 
+let chimpTwo = {
+  name: "Brad",
+  species: "Chimpanzee",
+  mass: 11,
+  age: 6
+};
+
+let beagle = {
+  name: "Leroy",
+  species: "Beagle",
+  mass: 14,
+  age: 5
+};
+
+let tardigrade = {
+  name:  "Almina",
+  species: "Tardigrade",
+  mass: 0.0000000001,
+  age: 1
+};
+
 
 // After you have created the other object literals, add the astronautID property to each one.
+let animals = [];
+animals.push(superChimpOne, chimpTwo, beagle, salamander, tardigrade);
+
+for (let i = 0; i < animals.length; i++) {
+  animals[i].astronautID = Math.floor(Math.random() * animals.length);
+
+  for (let j = 0; j < i; j++) {
+  while (animals[i].astronautID === animals[j].astronautID) {
+    animals[i].astronautID = Math.floor(Math.random() * animals.length);
+  }
+  }
+
+  console.log(animals[i].astronautID);
+};
 
 // Create an array to hold the animal objects.
 
