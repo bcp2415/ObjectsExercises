2db1a0713443af671a0bbf5588a7f98f87bb0bed
mprinceps <64213121+bcp2415@users.noreply.github.com>
2021-10-07 18:15:21 +0000

    adds unique generation of astronautID from 1 to 10; adds move method to each object.

--- index.js
+++ index.js
@@ -2,35 +2,50 @@ let superChimpOne = {
     name: "Chad",
     species: "Chimpanzee",
     mass: 9,
-    age: 6
+    age: 6,
+    move: function() {
+      return (Math.round(Math.random() * 10));
+    }
 };
 
 let salamander = {
     name: "Lacey",
     species: "Axolotl Salamander",
     mass: 0.1,
-    age: 5
+    age: 5,
+    move: function() {
+      return (Math.round(Math.random() * 10));
+    }
 };
 
 let chimpTwo = {
   name: "Brad",
   species: "Chimpanzee",
   mass: 11,
-  age: 6
+  age: 6,
+    move: function() {
+      return (Math.round(Math.random() * 10));
+    }
 };
 
 let beagle = {
   name: "Leroy",
   species: "Beagle",
   mass: 14,
-  age: 5
+  age: 5,
+    move: function() {
+      return (Math.round(Math.random() * 10));
+    }
 };
 
 let tardigrade = {
   name:  "Almina",
   species: "Tardigrade",
   mass: 0.0000000001,
-  age: 1
+  age: 1,
+    move: function() {
+      return (Math.round(Math.random() * 10));
+    }
 };
 
 
@@ -38,17 +53,27 @@ let tardigrade = {
 let animals = [];
 animals.push(superChimpOne, chimpTwo, beagle, salamander, tardigrade);
 
-for (let i = 0; i < animals.length; i++) {
-  animals[i].astronautID = Math.floor(Math.random() * animals.length);
+let astronautIDs = [];
 
-  for (let j = 0; j < i; j++) {
-  while (animals[i].astronautID === animals[j].astronautID) {
-    animals[i].astronautID = Math.floor(Math.random() * animals.length);
+function getNewId() {
+  return (Math.ceil(Math.random() * 10));
+}
+
+for (let i = 0; i < animals.length; i++) {
+  let newId = getNewId();
+
+  while (astronautIDs.includes(newId)) {
+    newId = getNewId();
   }
-  }
+
+  astronautIDs.push(newId);
+
+  animals[i].astronautID = newId;
 
   console.log(animals[i].astronautID);
 };
+
+
 
 // Create an array to hold the animal objects.
 
